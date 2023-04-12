2cab4984169e41877e90f4c0ea90ed34595cf41d
Denis Gabbasov <gabbas.denn@mail.ru>
2023-04-12 17:59:20 +0300

    feat:создать схему документа пользователя

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
-// Слушаем 3000 порт
+const mongoose = require("mongoose");
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
---
+++ models/card.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
