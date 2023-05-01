fb4da8c85c663bb23e6f1d5887df7434e12dc29c
Denis Gabbasov <gabbas.denn@mail.ru>
2023-05-01 14:17:31 +0300

    использовать валидацию joi для роутов карточек

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,6 @@
 const routerCard = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
 const {
   createCard,
   getCards,
@@ -7,14 +9,50 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
-routerCard.post('/', createCard);
+routerCard.post(
+  '/',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30),
+      link: Joi
+        .string()
+        .required()
+        .pattern(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/),
+    }),
+  }),
+  createCard,
+);
 
 routerCard.get('/', getCards);
 
-routerCard.delete('/:cardId', deleteCard);
+routerCard.delete(
+  '/:cardId',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().length(24).hex().required(),
+    }),
+  }),
+  deleteCard,
+);
 
-routerCard.put('/:cardId/likes', likeCard);
+routerCard.put(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().length(24).hex().required(),
+    }),
+  }),
+  likeCard,
+);
 
-routerCard.delete('/:cardId/likes', dislikeCard);
+routerCard.delete(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().length(24).hex().required(),
+    }),
+  }),
+  dislikeCard,
+);
 
 module.exports = routerCard;
