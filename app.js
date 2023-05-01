12ee61f8498efad79f5e8b4ba86e885222d464f2
Denis Gabbasov <gabbas.denn@mail.ru>
2023-05-01 12:07:00 +0300

    feat:использовать celebrate для валидации регистрации и логина

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 
 const { routerUsers, routerCards } = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
@@ -24,9 +25,31 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signup', createUser);
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string()
+        .pattern(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/),
+    }),
+  }),
+  createUser,
+);
 
-app.post('/signin', login);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
 
 // app.use(auth);
 
@@ -36,6 +59,8 @@ app.use('/cards', auth, routerCards);
 app.all('*', (req, res, next) => {
   next(new NotFoundError('Неверный адрес запроса'));
 });
+
+app.use(errors());
 
 app.use(errorHandler);
 
