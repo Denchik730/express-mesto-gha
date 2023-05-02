cc5a6b4bf1e961079f1d3f99e7f598b0d4e15b7e
Denis Gabbasov <gabbas.denn@mail.ru>
2023-05-02 05:51:59 +0300

    feat:релизовать роут для очищения токена из куков

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,10 @@ router.post(
   login,
 );
 
+router.get('/signout', auth, (req, res) => {
+  res.clearCookie('token').send({ message: 'Выход' });
+});
+
 router.use('/users', auth, routerUsers);
 router.use('/cards', auth, routerCards);
 
