3099fad2a1819a65ccc8223e99d2f97f6c07f49f
Denis Gabbasov <gabbas.denn@mail.ru>
2023-05-02 16:56:24 +0300

    feat:использовать модуль для ограничения количества запросов

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
 
 const errorHandler = require('./middlewares/errorHandler');
 const { router } = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const app = express();
 
@@ -17,6 +24,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   autoIndex: true,
 });
 
+app.use(limiter);
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
