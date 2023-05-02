a56f2fe1ab2e41e947cd388ded172c28ee1961c7
Denis Gabbasov <gabbas.denn@mail.ru>
2023-05-02 05:45:49 +0300

    feat:убрать все роуты в отдельный файл

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { celebrate, Joi, errors } = require('celebrate');
+const { errors } = require('celebrate');
 
-const { routerUsers, routerCards } = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
-const { NotFoundError } = require('./errors/NotFoundError');
-const { login } = require('./controllers/users');
-const { createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const { reg } = require('./utils/constants');
+const { router } = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
@@ -26,41 +21,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi
-        .string()
-        .pattern(reg),
-    }),
-  }),
-  createUser,
-);
-
-app.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login,
-);
-
-// app.use(auth);
-
-app.use('/users', auth, routerUsers);
-app.use('/cards', auth, routerCards);
-
-app.all('*', auth, (req, res, next) => {
-  next(new NotFoundError('Неверный адрес запроса'));
-});
+app.use(router);
 
 app.use(errors());
 
